496bf1971134ea2c78c10cc393bddf9ce9e94943
shedd <robert@shedd.us>
2013-12-03 23:08:05 -0800

    making the bash scripts dynamic

---
+++ graph.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# get the current directory of the script
+# see this page if your directory is a symlink: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ruby $DIR/instapaper-stats.rb csv backup >> $DIR/full.csv
+ruby $DIR/instapaper-stats.rb graph $DIR/full.csv
+cp -R $DIR/html/* $DIR/../public_html/instapaper/
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# get the current directory of the script
+# see this page if your directory is a symlink: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ruby $DIR/instapaper-stats.rb fetch $DIR/credentials.txt
